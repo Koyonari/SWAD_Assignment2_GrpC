f1902820b6b3d8836dc1fb5da80c3bd06b40eb87
kc <134157419+MetroBeann@users.noreply.github.com>
2024-08-07 02:55:08 +0800

    Addition of classes

---
+++ Accident.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD_Assignment2_GrpC
+{
+    internal class Accident
+    {
+        private string location;
+        private DateTime time;
+        private string type;
+        private Booking chosenBooking;
+
+        //Properties
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public DateTime Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public Booking ChosenBooking
+        {
+            get { return chosenBooking; }
+            set { chosenBooking = value; }
+        }
+
+        //Constructor
+        public Accident(string location, DateTime time, string type, Booking chosenBooking)
+        {
+            this.location = location;
+            this.time = time;
+            this.type = type;
+            this.chosenBooking = chosenBooking;
+        }
+    }
+}
---
+++ AppealPenalty.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD_Assignment2_GrpC
+{
+    internal class AppealPenalty
+    {
+        private string appealReason;
+        private DateTime appealDate;
+        private bool appealStatus;
+        private bool appealDecision;
+        private Penalty chosenPenalty;
+
+        //Properties
+        public string AppealReason
+        {
+            get { return appealReason; }
+            set { appealReason = value; }
+        }
+
+        public DateTime AppealDate
+        {
+            get { return appealDate; }
+            set { appealDate = value; }
+        }
+
+        public bool AppealStatus
+        {
+            get { return appealStatus; }
+            set { appealStatus = value; }
+        }
+
+        public bool AppealDecision
+        {
+            get { return appealDecision; }
+            set { appealDecision = value; }
+        }
+
+        public Penalty ChosenPenalty
+        {
+            get { return chosenPenalty; }
+            set { chosenPenalty = value; }
+        }
+
+        //Constructor
+        public AppealPenalty(string appealReason, DateTime appealDate, bool appealStatus, bool appealDecision, Penalty chosenPenalty)
+        {
+            this.appealReason = appealReason;
+            this.appealDate = appealDate;
+            this.appealStatus = appealStatus;
+            this.appealDecision = appealDecision;
+            this.chosenPenalty = chosenPenalty;
+        }
+    }
+}
