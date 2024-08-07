4b83607db382a77f0b25aca032897a4eac3f8dba
Heng-Github1 <https://github.com/Heng-Github1/2024_BED_Practicals>
2024-08-07 14:09:16 +0800

    fix: DriveLicense.cs

--- DriverLicense.cs
+++ DriverLicense.cs
@@ -1,90 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SWAD_Assignment2_GrpC
+﻿namespace SWAD_Assignment2_GrpC
 {
     internal class DriverLicense
     {
-        private int licenseId;
-        private string name;
-        private DateTime dateOfBirth;
-        private DateTime issueDate;
-        private DateTime expirationDate;
-        private string licenseType;
-        private string address;
-        private string status;
-        private int demeritPoints;
-
         // Properties
-        public int LicenseId
-        {
-            get { return licenseId; }
-            set { licenseId = value; }
-        }
+        public int LicenseId { get; set; }
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+        public string Name { get; set; }
 
-        public DateTime DateOfBirth
-        {
-            get { return dateOfBirth; }
-            set { dateOfBirth = value; }
-        }
+        public DateTime DateOfBirth { get; set; }
 
-        public DateTime IssueDate
-        {
-            get { return issueDate; }
-            set { issueDate = value; }
-        }
+        public DateTime IssueDate { get; set; }
 
-        public DateTime ExpirationDate
-        {
-            get { return expirationDate; }
-            set { expirationDate = value; }
-        }
+        public DateTime ExpirationDate { get; set; }
 
-        public string LicenseType
-        {
-            get { return licenseType; }
-            set { licenseType = value; }
-        }
+        public string LicenseType { get; set; }
 
-        public string Address
-        {
-            get { return address; }
-            set { address = value; }
-        }
+        public string Address { get; set; }
 
-        public string Status
-        {
-            get { return status; }
-            set { status = value; }
-        }
+        public string Status { get; set; }
 
-        public int DemeritPoints
-        {
-            get { return demeritPoints; }
-            set { demeritPoints = value; }
-        }
+        public int DemeritPoints { get; set; }
 
         // Constructor
         public DriverLicense(int licenseId, string name, DateTime dateOfBirth, DateTime issueDate, DateTime expirationDate, string licenseType, string address, string status, int demeritPoints)
         {
-            this.licenseId = licenseId;
-            this.name = name;
-            this.dateOfBirth = dateOfBirth;
-            this.issueDate = issueDate;
-            this.expirationDate = expirationDate;
-            this.licenseType = licenseType;
-            this.address = address;
-            this.status = status;
-            this.demeritPoints = demeritPoints;
+            LicenseId = licenseId;
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            IssueDate = issueDate;
+            ExpirationDate = expirationDate;
+            LicenseType = licenseType;
+            Address = address;
+            Status = status;
+            DemeritPoints = demeritPoints;
         }
     }
 }
