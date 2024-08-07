65ed8bd655ba3325ec0973cc2ae4006acf689db8
Heng-Github1 <https://github.com/Heng-Github1/2024_BED_Practicals>
2024-08-07 14:08:29 +0800

    fix: User Class

--- User.cs
+++ User.cs
@@ -1,74 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SWAD_Assignment2_GrpC
+﻿namespace SWAD_Assignment2_GrpC
 {
     internal class User
     {
-        private int id;
-        private string address;
-        private string email;
-        private string username;
-        private int contactNumber;
-        private string name;
-        private DateTime dateJoined;
-
         // Properties
-        public int Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
+        public int Id { get;set; }
 
-        public string Address
-        {
-            get { return address; }
-            set { address = value; }
-        }
+        public string Address { get; set; }
 
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
+        public string Email { get; set; }
 
-        public string Username
-        {
-            get { return username; }
-            set { username = value; }
-        }
+        public string Username { get; set; }
 
-        public int ContactNumber
-        {
-            get { return contactNumber; }
-            set { contactNumber = value; }
-        }
+        public int ContactNumber { get; set; }
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+        public string Name { get; set; }
 
-        public DateTime DateJoined
-        {
-            get { return dateJoined; }
-            set { dateJoined = value; }
-        }
+        public DateTime DateJoined { get; set; }
 
         // Constructor
         public User(int id, string address, string email, string username, int contactNumber, string name, DateTime dateJoined)
         {
-            this.id = id;
-            this.address = address;
-            this.email = email;
-            this.username = username;
-            this.contactNumber = contactNumber;
-            this.name = name;
-            this.dateJoined = dateJoined;
+            this.Id = id;
+            this.Address = address;
+            this.Email = email;
+            this.Username = username;
+            this.ContactNumber = contactNumber;
+            this.Name = name;
+            this.DateJoined = dateJoined;
         }
     }
 }
